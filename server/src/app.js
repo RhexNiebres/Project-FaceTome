a6ee7e742574422bc906cf43e49aa19f2742d3e5
RhexNiebres <rhexniebres@gmail.com>
2025-05-06 17:12:46 +0800

    chore: add initial file content

---
+++ server/src/app.js
@@ -0,0 +1,35 @@
+const dotenv = require("dotenv");
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+dotenv.config({ path: envFile });
+const express = require("express");
+const app = express();
+const indexRoutes = require("./routes/indexRoute");
+const authRoutes = require("./routes/authRoute");
+const usersRoutes = require("./routes/usersRoute");
+
+const cors = require("cors");
+
+const allowedOrigins = [process.env.CLIENT_HOST,process.env.MESSAGELY_CLIENT_HOST];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
+app.options("*", cors()); 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/", indexRoutes);
+app.use("/", authRoutes);
+app.use("/users", usersRoutes);
+
+
+app.listen(process.env.APP_PORT, () =>
+  console.log(`App listening on port ${process.env.APP_PORT}!`)
+);
