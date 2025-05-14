0dcb8000708f7fc0baf0e17e8abc38f3c1334617
RhexNiebres <rhexniebres@gmail.com>
2025-05-14 16:39:29 +0800

    chore: format using prettier

--- server/src/routes/commentsRoute.js
+++ server/src/routes/commentsRoute.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const commentController = require("../controllers/commentController");
-const { verifyToken } = require("../middlewares/verifyToken"); 
+const { verifyToken } = require("../middlewares/verifyToken");
 
-router.post("/:postId",verifyToken, commentController.createComment);
-router.delete("/:commentId",verifyToken,  commentController.deleteComment);
+router.post("/:postId", verifyToken, commentController.createComment);
+router.delete("/:commentId", verifyToken, commentController.deleteComment);
 
 module.exports = router;
