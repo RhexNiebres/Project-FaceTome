fc05eb7c6ddf82520e855a7fb7c936930e705d49
RhexNiebres <rhexniebres@gmail.com>
2025-05-14 16:39:45 +0800

    chore: format using prettier

--- server/src/routes/postRoute.js
+++ server/src/routes/postRoute.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
-const { verifyToken } = require("../middlewares/verifyToken"); 
-
+const { verifyToken } = require("../middlewares/verifyToken");
 
 router.get("/", postController.getAllPosts);
-router.post("/",verifyToken, postController.createPost);
-router.delete("/:id",verifyToken, postController.deletePost);
+router.post("/", verifyToken, postController.createPost);
+router.delete("/:id", verifyToken, postController.deletePost);
 
 module.exports = router;
