17a8f0e8a0cfe53bedb261506966d58bdda90a95
RhexNiebres <rhexniebres@gmail.com>
2025-05-16 14:45:34 +0800

    feat: add getAllPostsForCurrentUser api call to get all posts by current user

---
+++ client/facetome/src/apiServices/posts.js
@@ -0,0 +1,23 @@
+export const getAllPostsForCurrentUser = async () => {
+  try {
+    const response = await fetch(
+      import.meta.env.VITE_HOST + `/posts?isFollowingPostsInclude=false`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    if (!response.ok) {
+      const errorData = await response.json;
+      throw new Error(errorData?.error || "failed fetching user's posts");
+    }
+
+    const data = await response.json();
+    return { success: true, posts: data };
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+};
