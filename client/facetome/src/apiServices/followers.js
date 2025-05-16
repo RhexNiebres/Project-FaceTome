132b1bc8b521a9820df86fa0915fff42c10910f4
RhexNiebres <rhexniebres@gmail.com>
2025-05-16 16:48:30 +0800

    feat: add getFollowing api call to get all followers of current user

--- client/facetome/src/apiServices/followers.js
+++ client/facetome/src/apiServices/followers.js
@@ -1,7 +1,7 @@
 export const getFollowers = async (userId) => {
   try {
     const response = await fetch(
-      `${import.meta.env.VITE_HOST}/follow/${userId}/followers`,
+      import.meta.env.VITE_HOST + `/follow/${userId}/followers`,
       {
         method: "GET",
         headers: {
@@ -18,6 +18,31 @@ export const getFollowers = async (userId) => {
 
     const data = await response.json();
     return { success: true, followers: data };
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+};
+
+export const getFollowing = async (userId) => {
+  try {
+    const response = await fetch(
+      import.meta.env.VITE_HOST + `/follow/${userId}/following`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData?.error || "Failed to fetch following");
+    }
+
+    const data = await response.json();
+    return { success: true, following: data };
   } catch (error) {
     return { success: false, error: error.message };
   }
