8880bf62753f8656a946ec3c9d5c93be00559985
RhexNiebres <rhexniebres@gmail.com>
2025-05-16 13:48:00 +0800

    feat: add toggleLikePost api call to like or unlike a specific post

---
+++ client/facetome/src/apiServices/like.js
@@ -0,0 +1,23 @@
+export const toggleLikePost = async (postId) => {
+  try {
+    const response = await fetch(
+      import.meta.env.VITE_HOST + `/posts/${postId}/like`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData || "Failed to toggle like");
+    }
+    const data = await response.json;
+    return { success: true, message: data.message, like: data.like || null };
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+};
