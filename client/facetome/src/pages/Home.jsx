e5279b3a79552ec5450292ee872f55de53685ac6
RhexNiebres <rhexniebres@gmail.com>
2025-05-24 16:29:32 +0800

    feat: refactor: move all post fetching to Home and pass data to AllPost

--- client/facetome/src/pages/Home.jsx
+++ client/facetome/src/pages/Home.jsx
@@ -1,18 +1,43 @@
+import { useState, useEffect } from "react";
+import { getAllPosts } from "../apiServices/posts";
 import AllPost from "../components/AllPost";
 import CreatePost from "../components/CreatePost";
-import NavBar from "../components/NavBar"
+import NavBar from "../components/NavBar";
 
 const Home = () => {
-    return(
-        <div className="flex flex-col">
-        <NavBar/>
-        <div className="flex justify-center">
-        <CreatePost/>
-        </div>
-        
-        <AllPost/>
-        </div>
-    );
-}
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const result = await getAllPosts();
+
+      if (result.success) {
+        setPosts(result.posts);
+      }
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
+  const handlePostCreated = async () => {
+    const result = await getAllPosts();
+    if (result.success) {
+      setPosts(result.posts);
+    }
+  };
+
+  return (
+    <div className="flex flex-col bg-4 justify-center">
+      <NavBar />
+      <div className="flex justify-center">
+        <CreatePost onPostCreated={handlePostCreated} />
+      </div>
+      <div className="flex justify-center">
+        <AllPost posts={posts} loading={loading} />
+      </div>
+      
+    </div>
+  );
+};
 
 export default Home;
