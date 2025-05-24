8766da4339ae247342ebd4318470280265ca2bef
RhexNiebres <rhexniebres@gmail.com>
2025-05-24 16:28:04 +0800

    feat: show create post form when title input is clicked

--- client/facetome/src/components/CreatePost.jsx
+++ client/facetome/src/components/CreatePost.jsx
@@ -1,58 +1,81 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { createPost } from "../apiServices/posts";
-
-const CreatePost = () => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+const CreatePost = ({ onPostCreated }) => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [showForm, setShowForm] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-
+    setError(null);
     const result = await createPost({ title, content });
 
     if (result.success) {
       setTitle("");
       setContent("");
+      setShowForm(false)
       if (onPostCreated) onPostCreated(result.post);
     } else {
-      alert("Failed to create post");
+      alert(result.error || "Failed to create post");
     }
     setLoading(false);
   };
 
   return (
-    <div>
+    <div className="bg-2 p-4 m-4 text-white rounded w-1/2 flex justify-center">
       <form onSubmit={handleSubmit}>
-        <h2>Create New Post</h2>
-
-        <div>
-          <label>Title</label>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-          />
+        <label htmlFor="title" className="text-l font-bold">
+          Title
+        </label>
+        <input
+          id="title"
+          type="text"
+          value={title}
+          onClick={() => setShowForm(true)}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="What's on your mind?"
+          required
+          className="p-2 m-2 rounded-xl w-full text-gray-500 bg-1"
+        />
+        {showForm && (
+          <>
+          <label htmlFor="content" className="text-l font-bold">
+          Content
+        </label>
+        <textarea
+          id="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          rows="1"
+          required
+          className="p-2 m-2 rounded-xl w-full text-gray-500 bg-1"
+        />
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            disabled={loading}
+            className="p-2 m-2 bg-1 text-slate-200 hover:bg-2 rounded-xl "
+          >
+            {loading ? (
+              <div className="flex justify-center w-20">
+                <FontAwesomeIcon
+                  icon={faCircleNotch}
+                  spin
+                  className="text-blue-500 text-2xl"
+                />
+              </div>
+            ) : (
+              "Create Post"
+            )}
+          </button>
         </div>
-
-        <div>
-          <label>Content</label>
-          <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            rows="4"
-            required
-          />
-        </div>
-
-        <button 
-        type="submit"
-        disabled={loading}
-        >
-            {loading ? "posting..." : "Create Post"}
-        </button>
+        </>
+        )}
       </form>
     </div>
   );
