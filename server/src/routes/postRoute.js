48d1ce4663d5b88b1d19633c1149ce20afe6b707
RhexNiebres <rhexniebres@gmail.com>
2025-05-26 15:52:41 +0800

    chore: add post like route to post route file

--- server/src/routes/postRoute.js
+++ server/src/routes/postRoute.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postsController");
+const likeController = require("../controllers/likeController");
+const commentController = require("../controllers/commentsController");
 const { verifyToken } = require("../middlewares/verifyToken");
 
 router.get("/", verifyToken, postController.getAllPosts);
 router.post("/", verifyToken, postController.createPost);
 router.delete("/:id", verifyToken, postController.deletePost);
-
+router.post("/:postId/like", verifyToken, likeController.toggleLikePost);
+router.post("/:postId/", verifyToken, commentController.createComment);
 module.exports = router;
