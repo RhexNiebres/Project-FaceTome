01b53c1e7b3823143f7314c4416c4b6c6a9382b9
RhexNiebres <rhexniebres@gmail.com>
2025-05-26 16:18:06 +0800

    add find friends page route

--- client/facetome/src/AppRoutes.jsx
+++ client/facetome/src/AppRoutes.jsx
@@ -10,20 +10,23 @@ import LandingPage from "./pages/LandingPage";
 import NotFoundPage from "./pages/NotFound";
 import GoogleAuth from "./services/GoogleAuth";
 import UserProfilePage from "./pages/UserProfilePage";
+import FindFriendsPage from "./pages/FindFriendsPage";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route index element={<LandingPage />} />
+      <Route path="/" element={<App />}/>
+        <Route index element={<LandingPage />} />
 
+        <Route element={<ProtectedRoute />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<UserProfilePage />} />
+          <Route path="/community" element={<FindFriendsPage />} />
+        </Route>
+
+      
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-    <Route path="/oauth-success" element={<GoogleAuth />} />
-
-      <Route element={<ProtectedRoute />}>
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<UserProfilePage />} />
-      </Route>
+      <Route path="/oauth-success" element={<GoogleAuth />} />
 
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
