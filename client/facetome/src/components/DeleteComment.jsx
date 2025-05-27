d6718ab0cdf0a7476de2c2c89142d6c10da497df
RhexNiebres <rhexniebres@gmail.com>
2025-05-27 16:34:48 +0800

    feat: create component for deleting existing comments associated with a post

---
+++ client/facetome/src/components/DeleteComment.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { deleteComment } from "../apiServices/comments";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+const DeleteComment = ({ commentId, onDelete }) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleDelete = async () => {
+    setLoading(true);
+    setError(null);
+
+    const result = await deleteComment(commentId);
+
+    setLoading(false);
+    if (result.success) {
+      if (onDelete) {
+        onDelete(commentId);
+      }
+      alert("Comment Deleted");
+    } else {
+      setError(result.error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleDelete} disabled={loading} aria-label="Delete comment"       className={`text-sm flex items-center gap-1 transition-colors duration-200 ${
+          loading ? "text-gray-400 cursor-not-allowed" : "hover:text-red-500"
+        }`}>
+        {loading ? (
+          "Deleting..."
+        ) : (
+          <>
+            <FontAwesomeIcon icon={faTrash} className="hover:text-red-500" />
+          </>
+        )}
+      </button>
+       {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+    </div>
+  );
+};
+
+export default DeleteComment;
