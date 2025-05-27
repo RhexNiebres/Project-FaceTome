3c19c142bf52721e276027aeb14cd93e02fb9141
RhexNiebres <rhexniebres@gmail.com>
2025-05-27 16:32:48 +0800

    feat: add initial page content with username and status

---
+++ client/facetome/src/pages/FindFriendsPage.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import { getAllUsers } from "../apiServices/users";
+import NavBar from "../components/NavBar";
+
+const FindFriendsPage = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const result = await getAllUsers();
+
+      if (result.success) {
+        setUsers(result.users);
+      } else {
+        setError(result.error || "Failed to fetch users");
+      }
+      setLoading(false);
+    };
+    fetchUsers();
+  }, []);
+
+  if (loading) return <div>Loading users...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <div className="bg-4">
+      <NavBar />
+      <h1 className="text-gray-200 text-center font-bold p-6 text-xl">
+        Find Friends
+      </h1>
+      {users.length > 0 ? (
+        <div className="text-gray-200 p-2 flex flex-col items-center h-screen">
+          <ul>
+            {users.map((user) => {
+              let status = "Not Following";
+
+              if (user.isFollowingEachOther) {
+                status = "Mutual";
+              } else if (user.isFollowing) {
+                status = "Following";
+              } else if (user.isPendingRequest) {
+                status = "Pending";
+              } else if (user.hasCTA) {
+                status = "Requested";
+              } else if (user.canFollow) {
+                status = "";
+              }
+
+              return (
+                <li
+                  className="p-4 bg-1 w-96 rounded font-medium mb-3"
+                  key={user.id}
+                >
+                  <div className="text-white flex flex-col ">
+                    <span className="text-sm text-gray-300">{status}</span>
+                    <span>{user.username}</span>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      ) : (
+        <p>No Users Found </p>
+      )}
+    </div>
+  );
+};
+
+export default FindFriendsPage;
