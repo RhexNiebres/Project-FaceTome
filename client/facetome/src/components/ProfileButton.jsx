fb76ac636e8fecefb462c64119430f7f8d254c5b
RhexNiebres <rhexniebres@gmail.com>
2025-05-28 17:58:15 +0800

    refactor: moved function to display user avatar based on gender on parent component

--- client/facetome/src/components/ProfileButton.jsx
+++ client/facetome/src/components/ProfileButton.jsx
@@ -1,59 +1,29 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getUserById } from "../apiServices/users";
-
-const ProfileButton = () => {
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+const ProfileButton = ({ user, getAvatar }) => {
   const navigate = useNavigate();
 
-  const userId = localStorage.getItem("userId");
-
-  useEffect(() => {
-    if (!userId) {
-      setError("User not found in localStorage.");
-      return;
-    }
-
-    getUserById(userId).then((res) => {
-      if (res.success) {
-        setUser(res.user);
-      } else {
-        setError(res.error || "Failed to fetch user");
-      }
-    });
-  }, []);
-
-  const getAvatar = (gender) => {
-    switch (gender?.toLowerCase()) {
-      case "male":
-        return "/male.jpg";
-      case "female":
-        return "/female.jpg";
-      default:
-        return "/non_specified.jpg";
-    }
-  };
-
-  if (error) {
-    return <p className="text-red-500">{error}</p>;
-  }
-  
   return user ? (
     <button
       onClick={() => navigate("/profile")}
-      className="flex items-center space-x-2 bg-white text-blue-500 px-4 py-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-all duration-200 shadow-sm"
+      className="flex items-center space-x-2 bg-2 text-slate-200 px-4 py-2 rounded-md hover:bg-1 hover:text-slate-100 transition-all duration-200 shadow-sm"
     >
       <img
-        src={user.profilePicture ||getAvatar(user.gender)}
+        src={user.profilePicture || getAvatar(user.gender)}
         alt="User avatar"
         className="w-8 h-8 rounded-full object-cover"
-       
-      />{/* fixed 429 error with loading= lazy to prevent immediate request to google*/}
-      <span className=" font-bold">{user.username}</span>
+      />
+      <span>{user.username}</span>
     </button>
   ) : (
-    <p>Loading...</p>
+    <div className="flex justify-center items-center p-10">
+      <FontAwesomeIcon
+        icon={faCircleNotch}
+        spin
+        className="text-blue-500 text-3xl"
+      />
+    </div>
   );
 };
 
