988467f95a5adb37c918da22e522e21b2f97b3ff
RhexNiebres <rhexniebres@gmail.com>
2025-06-06 19:14:55 +0800

    fix: show only title input before form expands

--- client/facetome/src/components/CreatePost.jsx
+++ client/facetome/src/components/CreatePost.jsx
@@ -28,22 +28,32 @@ const CreatePost = ({ onPostCreated }) => {
   };
 
   return (
-    <div className="bg-2 p-4 m-4 text-white rounded-xl w-1/2 flex justify-center shadow-xl">
+    <div className="bg-2 p-4 m-4 text-white rounded-xl w-1/2  shadow-xl">
       <form onSubmit={handleSubmit}>
-        <label htmlFor="title" className="text-l font-bold">
-        </label>
-        <input
-          id="title"
-          type="text"
-          value={title}
-          onClick={() => setShowForm(true)}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="What's on your mind?"
-          required
-          className="p-2 m-2 rounded-xl w-full text-4 bg-gray-100"
-        />
-        {showForm && (
+        {!showForm ? (
+          <input
+            id="title"
+            type="text"
+            value={title}
+            onClick={() => setShowForm(true)}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="What's on your mind?"
+            required
+            className="p-2 m-2 rounded-xl w-full text-4 bg-gray-100"
+          />
+        ) : (
           <>
+            <label htmlFor="title" className="text-l font-bold">
+              Title
+            </label>
+            <input
+              id="title"
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+              className="p-2 m-2 rounded-xl w-full text-4 bg-gray-100"
+            />
             <label htmlFor="content" className="text-l font-bold pl-3">
               Content
             </label>
